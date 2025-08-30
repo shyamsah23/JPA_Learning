a650412a6e8d542fbc18cf6f564805b15eef571d
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2025-08-31 00:33:30 +0530

    Revert "Logger added"

--- src/main/java/com/Learner/hospitalManagement/controller/AuthController.java
+++ src/main/java/com/Learner/hospitalManagement/controller/AuthController.java
@@ -7,8 +7,7 @@ import com.Learner.hospitalManagement.dto.SignupRequestDto;
 import com.Learner.hospitalManagement.dto.SignupResponseDto;
 import com.Learner.hospitalManagement.entity.User;
 import com.Learner.hospitalManagement.security.AuthService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,14 +16,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
-    
     @Autowired
     private AuthService authService;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
-        logger.info("Inside Login Controller");
+
         return ResponseEntity.ok(authService.login(loginRequestDto));
     }
 
