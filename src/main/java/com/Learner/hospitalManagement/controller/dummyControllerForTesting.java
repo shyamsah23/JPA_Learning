47c8f8bf39981425ad5d19ea2695b17287844735
Ayush <jhanathayush@gmail.com>
2025-09-07 11:10:02 +0530

    JIRA-123-Tested On Local

--- src/main/java/com/Learner/hospitalManagement/controller/dummyControllerForTesting.java
+++ src/main/java/com/Learner/hospitalManagement/controller/dummyControllerForTesting.java
@@ -1,14 +1,16 @@
 package com.Learner.hospitalManagement.controller;
 
 
+import com.Learner.hospitalManagement.customAnnotation.TrackTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class dummyControllerForTesting {
-
+    @TrackTime
     @GetMapping("/dummy")
-    public String dummyController(){
+    public String dummyController() throws InterruptedException {
+        Thread.sleep(4000);
         return "Hello World";
     }
 }
